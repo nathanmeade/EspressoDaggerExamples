5dab7a295908a4f5a17f32a78ef06fd66dbd1c6c
Nathan Meade <nathan.r.meade@gmail.com>
2020-06-18 11:51:10 -0400

    feat: TestAppComponent, TestBaseApplication and MockTestRunner  implemented

---
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/DetailFragmentTest.kt
@@ -0,0 +1,15 @@
+package com.codingwithmitch.espressodaggerexamples.ui
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class DetailFragmentTest {
+
+    @Test
+    fun doNothing(){
+        assertEquals(1, 1)
+    }
+}
