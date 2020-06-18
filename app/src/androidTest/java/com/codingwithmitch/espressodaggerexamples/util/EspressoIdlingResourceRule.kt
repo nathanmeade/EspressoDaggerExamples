56a62fda5e38fff92a0609adf213ec7b96a60f75
Nathan Meade <nathan.r.meade@gmail.com>
2020-06-18 15:01:54 -0400

    feat: EspressoIdlingResource implemented

---
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/util/EspressoIdlingResourceRule.kt
@@ -0,0 +1,20 @@
+package com.codingwithmitch.espressodaggerexamples.util
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class EspressoIdlingResourceRule : TestWatcher(){
+    private val idlingResource = EspressoIdlingResource.countingIdlingResource
+
+    override fun finished(description: Description?) {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        super.finished(description)
+    }
+
+    override fun starting(description: Description?) {
+        IdlingRegistry.getInstance().register(idlingResource)
+        super.starting(description)
+    }
+}
